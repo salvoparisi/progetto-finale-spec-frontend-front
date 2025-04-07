8dd3ce926bc8fd8cae8f89dbe4e652512993ad9a
salvoparisi <psalvo065@gmail.com>
2025-04-08 01:59:26 +0200

    add category and sort

--- src/pages/DevicePage.jsx
+++ src/pages/DevicePage.jsx
@@ -6,6 +6,8 @@ export default function DevicePage() {
     const { apiUrl } = useContext(GlobalContext)
     const [devices, setDevices] = useState([])
     const [filterName, setFilterName] = useState("")
+    const [category, setCategory] = useState('')
+    const [sort, setSort] = useState(1)
 
     useEffect(() => {
         const urls = [
@@ -20,12 +22,38 @@ export default function DevicePage() {
             })
     }, [filterName])
 
+    const categoryDevice = useMemo(() => {
+        if (category === '') return devices
+        return devices.filter(obj => obj.category === category)
+    }, [category, devices])
 
+    function handleSort() {
+        if (sort === 1) setSort(-1)
+        if (sort === -1) setSort(1)
+    }
+
+    const sortedTask = useMemo(() => {
+        return [...categoryDevice].sort((a, b) => {
+            return a.title.localeCompare(b.title) * sort
+        })
+    }, [sort, categoryDevice])
 
     return (
         <div>
-            <input type="text" onChange={(e) => setFilterName(e.target.value)} />
-            <CardLayout arrayObj={devices} />
+            <div className="d-flex">
+                <input type="text" onChange={(e) => setFilterName(e.target.value)} />
+                <div>
+                    Categoria:
+                    <select name="" id="" onChange={(e) => setCategory(e.target.value)}>
+                        <option value="">Tutti i dispositivi</option>
+                        <option value="Smartphone">Smartphone</option>
+                        <option value="Tablet">Tablet</option>
+                        <option value="Smartwatch">Smartwatch</option>
+                    </select>
+                </div>
+                <button onClick={handleSort} className="btn btn-outline-secondary">{sort === 1 ? "↓" : "↑"}</button>
+            </div>
+            <CardLayout arrayObj={sortedTask} />
         </div>
     )
 }
